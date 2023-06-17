910b97eab5b4c36668e882424de50328ed8e3284
almaz <almazgimaev@gmail.com>
2023-06-18 00:41:58 +0300

    add main script for export videos after review

--- src/main.py
+++ src/main.py
@@ -1,93 +1,81 @@
-import json, os
+import os
 import supervisely as sly
 
 from dotenv import load_dotenv
 from tqdm import tqdm
+from supervisely.video_annotation.key_id_map import KeyIdMap
 
-# load ENV variables for debug, has no effect in production
-load_dotenv("local.env")
-load_dotenv(os.path.expanduser("~/supervisely.env"))
+if sly.is_development():
+    load_dotenv("local.env")
+    load_dotenv(os.path.expanduser("~/supervisely.env"))
 
+api = sly.Api.from_env()
 
-STORAGE_DIR = sly.app.get_data_dir() # path to directory for temp files and result archive
-ANN_FILE_NAME = "labels.json"
+JOB_ID = os.environ.get("modal.state.slyJobId")
 
-
-class MyExport(sly.app.Export):
-    def process(self, context: sly.app.Export.Context):
-        # create api object to communicate with Supervisely Server
-        api = sly.Api.from_env()
-
-        # get project info from server
-        project_info = api.project.get_info_by_id(id=context.project_id)
-
-        # make project directory path
-        data_dir = os.path.join(STORAGE_DIR, f"{project_info.id}_{project_info.name}")
-
-        # get project meta
-        meta_json = api.project.get_meta(id=context.project_id)
-        project_meta = sly.ProjectMeta.from_json(meta_json)
-
-        # Check if the app runs from the context menu of the dataset. 
-        if context.dataset_id is not None:
-            # If so, get the dataset info from the server.
-            dataset_infos = [api.dataset.get_info_by_id(context.dataset_id)]
-        else:
-            # If it does not, obtain all datasets infos from the current project.
-            dataset_infos = api.dataset.get_list(context.project_id)
-
-        # iterate over datasets in project
-        for dataset in dataset_infos:
-            result_anns = {}
-
-            # get dataset images info
-            images_infos = api.image.get_list(dataset.id)
-
-            # track progress using Tqdm
-            with tqdm(total=dataset.items_count) as pbar:
-                # iterate over images in dataset
-                for image_info in images_infos:
-                    labels = []
-
-                    # create path for each image and download it from server
-                    image_path = os.path.join(data_dir, dataset.name, image_info.name)
-                    api.image.download(image_info.id, image_path)
-
-                    # download annotation for current image
-                    ann_json = api.annotation.download_json(image_info.id)
-                    ann = sly.Annotation.from_json(ann_json, project_meta)
-
-                    # iterate over labels in current annotation
-                    for label in ann.labels:
-                        # get obj class name
-                        name = label.obj_class.name
-
-                        # get bounding box coordinates for label
-                        bbox = label.geometry.to_bbox()
-                        labels.append(
-                            {
-                                "class_name": name,
-                                "coordinates": [
-                                    bbox.top,
-                                    bbox.left,
-                                    bbox.bottom,
-                                    bbox.right,
-                                ],
-                            }
-                        )
-
-                    result_anns[image_info.name] = labels
-
-                    # increment the current progress counter by 1
-                    pbar.update(1)
-
-            # create JSON annotation in new format
-            filename = os.path.join(data_dir, dataset.name, ANN_FILE_NAME)
-            with open(filename, "w") as file:
-                json.dump(result_anns, file, indent=2)
-
-        return data_dir
+app = sly.Application()
 
 
-app = MyExport()
-app.run()
+if JOB_ID is None:
+    raise RuntimeError("Job id is not specified")
+
+job = api.labeling_job.get_info_by_id(JOB_ID)
+project = api.project.get_info_by_id(job.project_id)
+dataset = api.dataset.get_info_by_id(job.dataset_id)
+
+if job is None or dataset is None or project is None:
+    raise RuntimeError("Job, project or dataset not found")
+
+reviewed_video_ids = [video["id"] for video in job.entities if video["reviewStatus"] == "accepted"]
+sly.logger.info(f"Found {len(reviewed_video_ids)} reviewed videos")
+
+# make project directory path
+STORAGE_DIR = sly.app.get_data_dir()
+project_dir = os.path.join(STORAGE_DIR, f"{project.id}_{project.name}")
+
+# get project meta
+meta_json = api.project.get_meta(id=project.id)
+project_meta = sly.ProjectMeta.from_json(meta_json)
+
+# create local project and dataset
+key_id_map = KeyIdMap()
+project_fs = sly.VideoProject(project_dir, sly.OpenMode.CREATE)
+project_fs.set_meta(project_meta)
+dataset_fs = project_fs.create_dataset(dataset.name)
+
+# get reviewed videos infos
+videos = api.video.get_list(
+    dataset.id,
+    filters=[{"field": "id", "operator": "in", "value": reviewed_video_ids}],
+)
+
+
+progress = tqdm(total=len(videos), desc=f"Downloading videos...")
+for batch in sly.batched(videos, batch_size=10):
+    video_ids = [video_info.id for video_info in batch]
+    video_names = [video_info.name for video_info in batch]
+    ann_jsons = api.video.annotation.download_bulk(dataset.id, video_ids)
+    for video_id, video_name, ann_json in zip(video_ids, video_names, ann_jsons):
+        video_ann = sly.VideoAnnotation.from_json(ann_json, project_meta, key_id_map)
+
+        video_file_path = dataset_fs.generate_item_path(video_name)
+        api.video.download_path(video_id, video_file_path)
+        dataset_fs.add_item_file(video_name, video_file_path, ann=video_ann, _validate_item=False)
+
+    progress.update(len(batch))
+
+project_fs.set_key_id_map(key_id_map)
+sly.output.set_download(project_dir)
+
+
+reviewed = len(reviewed_video_ids)
+not_reviewed = dataset.items_count - reviewed
+sly.logger.info(
+    f"""
+Dataset {dataset.name} has {dataset.items_count} videos:
+    * {reviewed} reviewed videos - processed;
+    * {not_reviewed} not reviewed videos - skipped.
+"""
+)
+
+app.stop()
