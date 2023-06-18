33deb162caea8e7d2a66728446425ac51bb0302c
almaz <almazgimaev@gmail.com>
2023-06-18 11:55:14 +0300

    add export_videos func

---
+++ src/export_videos.py
@@ -0,0 +1,44 @@
+from typing import List
+import supervisely as sly
+from supervisely.video_annotation.key_id_map import KeyIdMap
+
+from tqdm import tqdm
+
+
+def export_videos(
+    api: sly.Api,
+    dataset: sly.Dataset,
+    reviewed_item_ids: List[int],
+    project_dir: str,
+    project_meta: sly.ProjectMeta,
+):
+    # create local project and dataset
+    key_id_map = KeyIdMap()
+    project_fs = sly.VideoProject(project_dir, sly.OpenMode.CREATE)
+    project_fs.set_meta(project_meta)
+    dataset_fs = project_fs.create_dataset(dataset.name)
+
+    # get reviewed videos infos
+    videos = api.video.get_list(
+        dataset.id,
+        filters=[{"field": "id", "operator": "in", "value": reviewed_item_ids}],
+    )
+
+    progress = tqdm(total=len(videos), desc=f"Downloading videos...")
+    for batch in sly.batched(videos, batch_size=10):
+        video_ids = [video_info.id for video_info in batch]
+        video_names = [video_info.name for video_info in batch]
+        ann_jsons = api.video.annotation.download_bulk(dataset.id, video_ids)
+        for video_id, video_name, ann_json in zip(video_ids, video_names, ann_jsons):
+            video_ann = sly.VideoAnnotation.from_json(ann_json, project_meta, key_id_map)
+
+            video_file_path = dataset_fs.generate_item_path(video_name)
+            api.video.download_path(video_id, video_file_path)
+            dataset_fs.add_item_file(
+                video_name, video_file_path, ann=video_ann, _validate_item=False
+            )
+
+        progress.update(len(batch))
+
+    project_fs.set_key_id_map(key_id_map)
+    sly.output.set_download(project_dir)
--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ import supervisely as sly
 
 from dotenv import load_dotenv
 from tqdm import tqdm
-from supervisely.video_annotation.key_id_map import KeyIdMap
+
+from src.export_videos import export_videos
 
 if sly.is_development():
     load_dotenv("local.env")
@@ -17,7 +18,7 @@ app = sly.Application()
 
 
 if JOB_ID is None:
-    raise RuntimeError("Job id is not specified")
+    raise RuntimeError("Job ID is not specified")
 
 job = api.labeling_job.get_info_by_id(JOB_ID)
 project = api.project.get_info_by_id(job.project_id)
@@ -26,8 +27,8 @@ dataset = api.dataset.get_info_by_id(job.dataset_id)
 if job is None or dataset is None or project is None:
     raise RuntimeError("Job, project or dataset not found")
 
-reviewed_video_ids = [video["id"] for video in job.entities if video["reviewStatus"] == "accepted"]
-sly.logger.info(f"Found {len(reviewed_video_ids)} reviewed videos")
+reviewed_item_ids = [video["id"] for video in job.entities if video["reviewStatus"] == "accepted"]
+sly.logger.info(f"Found {len(reviewed_item_ids)} reviewed videos")
 
 # make project directory path
 STORAGE_DIR = sly.app.get_data_dir()
@@ -37,44 +38,19 @@ project_dir = os.path.join(STORAGE_DIR, f"{project.id}_{project.name}")
 meta_json = api.project.get_meta(id=project.id)
 project_meta = sly.ProjectMeta.from_json(meta_json)
 
-# create local project and dataset
-key_id_map = KeyIdMap()
-project_fs = sly.VideoProject(project_dir, sly.OpenMode.CREATE)
-project_fs.set_meta(project_meta)
-dataset_fs = project_fs.create_dataset(dataset.name)
 
-# get reviewed videos infos
-videos = api.video.get_list(
-    dataset.id,
-    filters=[{"field": "id", "operator": "in", "value": reviewed_video_ids}],
-)
+if project.type == str(sly.ProjectType.VIDEOS):
+    export_videos(api, dataset, reviewed_item_ids, project_dir, project_meta)
 
 
-progress = tqdm(total=len(videos), desc=f"Downloading videos...")
-for batch in sly.batched(videos, batch_size=10):
-    video_ids = [video_info.id for video_info in batch]
-    video_names = [video_info.name for video_info in batch]
-    ann_jsons = api.video.annotation.download_bulk(dataset.id, video_ids)
-    for video_id, video_name, ann_json in zip(video_ids, video_names, ann_jsons):
-        video_ann = sly.VideoAnnotation.from_json(ann_json, project_meta, key_id_map)
-
-        video_file_path = dataset_fs.generate_item_path(video_name)
-        api.video.download_path(video_id, video_file_path)
-        dataset_fs.add_item_file(video_name, video_file_path, ann=video_ann, _validate_item=False)
-
-    progress.update(len(batch))
-
-project_fs.set_key_id_map(key_id_map)
-sly.output.set_download(project_dir)
-
-
-reviewed = len(reviewed_video_ids)
+reviewed = len(reviewed_item_ids)
 not_reviewed = dataset.items_count - reviewed
+items_type = " ".join(project.type.split("_"))
 sly.logger.info(
     f"""
-Dataset {dataset.name} has {dataset.items_count} videos:
-    * {reviewed} reviewed videos - processed;
-    * {not_reviewed} not reviewed videos - skipped.
+Dataset {dataset.name} has {dataset.items_count} {items_type}:
+    * {reviewed} reviewed {items_type} - processed;
+    * {not_reviewed} not reviewed {items_type} - skipped.
 """
 )
 
